94ee233ea027630c6340a35f486d5933cd005db1
Samuel <samuelrivba@gmail.com>
2024-04-10 15:18:37 -0500

    Add array bit

--- types.js
+++ types.js
@@ -1,7 +1,24 @@
+// Primitive
 // Numbers 5
 // Booleans true
 // Strings 'To be or not to be'
 // undefined
 // null
 // Symbol("just me")
+
+// Non-Primitive
 // Objects {}
+// Arrays that are objects
+// functions that are objects aswell
+
+// JS wraps primitives when trying to access a property
+console.log(true.toString());
+console.log(true);
+
+var array = [1, 2, 3];
+// =
+var arrayObj = {
+  0: 1,
+  1: 2,
+  2: 3,
+};
